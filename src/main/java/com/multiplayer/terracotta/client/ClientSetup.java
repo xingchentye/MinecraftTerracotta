402fcc71d16baef298c554d80698949fc137fddb
黄雯翔 <xingchenawa@qq.com>
2026-01-18 00:34:17 +0800

    feat(client): show clickable room code in chat and widen toast

--- src/main/java/com/multiplayer/terracotta/client/ClientSetup.java
+++ src/main/java/com/multiplayer/terracotta/client/ClientSetup.java
@@ -4,15 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.multiplayer.terracotta.Config;
 import com.multiplayer.terracotta.client.gui.StartupScreen;
-import com.multiplayer.terracotta.logic.ProcessLauncher;
 import com.multiplayer.terracotta.network.TerracottaApiClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -27,11 +32,13 @@ import net.neoforged.neoforge.client.event.ScreenEvent;
  * @author xingchentye
  */
 public class ClientSetup {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientSetup.class);
     private static boolean hasAutoStarted = false;
     private static int tickCounter = 0;
     private static boolean wasHostOk = false;
     private static String lastRoomCode = "";
     private static final Gson GSON = new Gson();
+    private static String lastStateValue = "";
 
     private static class SimpleToast implements Toast {
         private final Component title;
@@ -65,7 +72,7 @@ public class ClientSetup {
 
         @Override
         public int width() {
-            return 160;
+            return 220;
         }
 
         @Override
@@ -77,6 +84,23 @@ public class ClientSetup {
     public static void showToast(Component title, Component message) {
         Minecraft mc = Minecraft.getInstance();
         mc.getToasts().addToast(new SimpleToast(title, message));
+    }
+
+    private static void showRoomCodeInChat(String roomCode) {
+        Minecraft mc = Minecraft.getInstance();
+        MutableComponent msg = Component.literal("[Terracotta] 房间已创建！房间号: ");
+        msg.withStyle(ChatFormatting.GREEN);
+
+        MutableComponent code = Component.literal(roomCode);
+        code.withStyle(style -> style
+            .withColor(ChatFormatting.AQUA)
+            .withBold(true)
+            .withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, roomCode))
+            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("点击复制")))
+        );
+
+        msg.append(code);
+        mc.gui.getChat().addMessage(msg);
     }
 
     /**
@@ -102,6 +126,9 @@ public class ClientSetup {
      */
     private static void checkBackendState() {
         if (!TerracottaApiClient.hasDynamicPort()) {
+            if (wasHostOk) {
+                LOGGER.info("Backend dynamic port lost while previously host-ok, room considered closed");
+            }
             wasHostOk = false;
             lastRoomCode = "";
             return;
@@ -113,18 +140,25 @@ public class ClientSetup {
                 JsonObject json = GSON.fromJson(stateJson, JsonObject.class);
                 if (json.has("state")) {
                     String state = json.get("state").getAsString();
+                    if (!state.equals(lastStateValue)) {
+                        LOGGER.info("Terracotta backend state changed: {} -> {}", lastStateValue, state);
+                        lastStateValue = state;
+                    }
                     boolean isHostOk = "host-ok".equals(state);
 
                     if (isHostOk) {
                         if (json.has("room")) {
                             String roomCode = json.get("room").getAsString();
-                            // 如果是从非 host-ok 状态转变而来，或者房间号发生了变化
                             if (!wasHostOk || !roomCode.equals(lastRoomCode)) {
                                 lastRoomCode = roomCode;
+                                LOGGER.info("Host room ready with code {}", roomCode);
                                 showRoomCodeToasts(roomCode);
                             }
                         }
                     } else {
+                        if (wasHostOk) {
+                            LOGGER.info("Host room left host-ok state, new state={}", state);
+                        }
                         lastRoomCode = "";
                     }
                     wasHostOk = isHostOk;
@@ -137,6 +171,7 @@ public class ClientSetup {
         Minecraft minecraft = Minecraft.getInstance();
         minecraft.execute(() -> {
             showToast(Component.literal("房间号"), Component.literal("房间号: " + roomCode));
+            showRoomCodeInChat(roomCode);
             try {
                 minecraft.keyboardHandler.setClipboard(roomCode);
                 showToast(Component.literal("提示"), Component.literal("房间号已复制到剪贴板"));
